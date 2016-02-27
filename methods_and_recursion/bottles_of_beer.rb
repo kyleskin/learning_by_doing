8145587f25776a5e2fd3a3d53c64dbb1390de389
Kyle Skinner <kyleskin@gmail.com>
2016-02-27 09:41:20 -0700

    Bottles of Beer
    * Correct lyric
    
    [#114622833]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -4,15 +4,15 @@ def bottles_of_beer(number)
 
   if number >= 3
     puts "#{number.to_s} bottles of beer on the wall, #{number.to_s} bottles of beer."
-    puts "Take one down, pass it around, #{((number) - 1)} bottles of beer on the wall."
+    puts "Take one down and pass it around, #{((number) - 1)} bottles of beer on the wall."
     puts ''
   elsif number == 2
     puts "#{number.to_s} bottles of beer on the wall, #{number.to_s} bottles of beer."
-    puts "Take one down, pass it around, #{((number) - 1)} bottle of beer on the wall."
+    puts "Take one down and pass it around, #{((number) - 1)} bottle of beer on the wall."
     puts ''
   elsif number == 1
     puts "#{number.to_s} bottle of beer on the wall, #{number.to_s} bottle of beer."
-    puts "Take one down, pass it around, no more bottles of beer on the wall."
+    puts "Take one down and pass it around, no more bottles of beer on the wall."
     puts ''
   else
     puts 'No more bottles of beer on the wall, no more bottles of beer.'
