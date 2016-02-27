d8e83ebe202ca8248940ed79f0ff76317ea036eb
Kyle Skinner <kyleskin@gmail.com>
2016-02-27 09:17:26 -0700

    Bottles of Beer
    * Added code for 0 bottles
    
    [#114622833]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -10,14 +10,15 @@ def bottles_of_beer(number)
     puts "#{number.to_s} bottles of beer on the wall, #{number.to_s} bottles of beer."
     puts "Take one down, pass it around, #{((number) - 1)} bottle of beer on the wall."
     puts ''
-  else
+  elsif number == 1
     puts "#{number.to_s} bottle of beer on the wall, #{number.to_s} bottle of beer."
     puts "Take one down, pass it around, no more bottles of beer on the wall."
     puts ''
+  else
     puts 'No more bottles of beer on the wall, no more bottles of beer.'
     puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
   end
-  if number > 1
+  if number > 0
     bottles_of_beer(number - 1)
   end
 end
