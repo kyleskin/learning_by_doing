978571b162a3b26adc00857ef98151938c5d5937
Kyle Skinner <kyleskin@gmail.com>
2016-02-27 10:54:07 -0700

    Tweaking code to try to pass homework tester
    [#114614247]

--- methods_and_recursion/mad_libs.rb
+++ methods_and_recursion/mad_libs.rb
@@ -25,22 +25,21 @@ another_adjective_2 = ask_question('nother adjective')
 number = ask_question(' number')
 another_plural_noun_3 = ask_question('nother plural noun')
 
-puts <<-TEXT
-Here is your madlib:
-A vacation is when you take a trip to some #{adjective} place
-with your #{another_adjective} family.  Usually you go to some place
-that is near a/an #{noun} or up on a/an #{another_noun}.
-A good vacation place is one where you can ride #{plural_noun}
-or play #{game} or go hunting for #{another_plural_noun}.  I like
-to spend my time #{progressive_verb} or #{another_progressive_verb}.
-When parents go on a vacation, they spend their time eating
-three #{another_plural_noun_2} a day, and fathers play golf, and mothers
-sit around #{another_progressive_verb_2}.  Last summer, my little brother
-fell in a/an #{another_noun_2} and got poison #{plant} all
-over his #{body_part}.  My family is going to go to (the)
-#{place}, and I will practice #{another_progressive_verb_3}.  Parents
-need vacation more than kids because parents are always very
-#{another_adjective_2} and because they have to work #{number}
-hours every day all year making enough #{another_plural_noun_3} to pay
-for the vacation.
-TEXT
+puts 'Here is your madlib:'
+puts "A vacation is when you take a trip to some #{adjective} place"
+puts "with your #{another_adjective} family.  Usually you go to some place"
+puts "that is near a/an #{noun} or up on a/an #{another_noun}."
+puts "A good vacation place is one where you can ride #{plural_noun}"
+puts "or play #{game} or go hunting for #{another_plural_noun}.  I like"
+puts "to spend my time #{progressive_verb} or #{another_progressive_verb}."
+puts 'When parents go on a vacation, they spend their time eating'
+puts "three #{another_plural_noun_2} a day, and fathers play golf, and mothers"
+puts "sit around #{another_progressive_verb_2}.  Last summer, my little brother"
+puts "fell in a/an #{another_noun_2} and got poison #{plant} all"
+puts "over his #{body_part}.  My family is going to go to (the)"
+puts "#{place}, and I will practice #{another_progressive_verb_3}.  Parents"
+puts 'need vacation more than kids because parents are always very'
+puts "#{another_adjective_2} and because they have to work #{number}"
+puts "hours every day all year making enough #{another_plural_noun_3} to pay"
+puts 'for the vacation.'
+
