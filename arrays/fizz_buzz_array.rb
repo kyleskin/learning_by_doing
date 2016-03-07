f5ce9944e1b29ef250ca92e7944739d97e0ef7e8
Kyle Skinner <kyleskin@gmail.com>
2016-03-06 19:43:47 -0700

    Array FizzBuzz
    * Making changes to redo code
    
    [#114623807]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -2,15 +2,28 @@
 
 numbers = (1..100).to_a
 
-numbers.each {|num|
-  if (num % 3 == 0) && (num % 5 == 0)
-    puts 'FizzBuzz'
-  elsif num % 3 == 0
-    puts 'Fizz'
-  elsif num % 5 == 0
-    puts 'Buzz'
+def fizzbuzz(num)
+  string = ''
+  if num % 3 == 0
+    string += 'Fizz'
+    if num % 5 == 0
+      string += 'Buzz'
+    end
   else
-    puts num
+    string += num
   end
-}
+end
 
+numbers.map{ |x| x.fizzbuzz }
+
+# numbers.each {|num|
+#   if (num % 3 == 0) && (num % 5 == 0)
+#     puts 'FizzBuzz'
+#   elsif num % 3 == 0
+#     puts 'Fizz'
+#   elsif num % 5 == 0
+#     puts 'Buzz'
+#   else
+#     puts num
+#   end
+# }
