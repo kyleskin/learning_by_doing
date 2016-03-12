d461e4aa5f3e6cafa1b89d2f96d8137458d4cf26
Kyle Skinner <kyleskin@gmail.com>
2016-03-12 14:42:22 -0700

    FizzBuzz Array
    * TA suggested refactoring
    
    [#114623807]

--- arrays/fizz_buzz_array.rb
+++ arrays/fizz_buzz_array.rb
@@ -1,29 +1,18 @@
 #!/usr/bin/env ruby
 
-numbers = (1..100).to_a
+array_of_numbers = (1..100).to_a
 
-def fizzbuzz(num)
-  string = ''
-  if num % 3 == 0
-    string += 'Fizz'
-    if num % 5 == 0
-      string += 'Buzz'
-    end
+def fizzbuzzer(num)
+  if (num % 3 == 0) && (num % 5 == 0)
+    puts 'FizzBuzz'
+  elsif num % 3 == 0
+    puts 'Fizz'
+  elsif num % 5 == 0
+    puts 'Buzz'
   else
-    string += num
+    puts num
   end
 end
 
-numbers.map{ |x| x.fizzbuzz }
-
-# numbers.each {|num|
-#   if (num % 3 == 0) && (num % 5 == 0)
-#     puts 'FizzBuzz'
-#   elsif num % 3 == 0
-#     puts 'Fizz'
-#   elsif num % 5 == 0
-#     puts 'Buzz'
-#   else
-#     puts num
-#   end
-# }
+fizzbuzz_result = array_of_numbers.map { |i| fizzbuzzer(i) }
+puts fizzbuzz_result
