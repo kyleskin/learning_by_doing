b7d533009df72a92c67a09721de54741af660460
Kyle Skinner <kyleskin@gmail.com>
2016-03-12 19:06:40 -0700

    Fishing Pole
    * Corrected rubocop errors
    
    [#114616043]

--- objects/fishing_pole.rb
+++ objects/fishing_pole.rb
@@ -2,13 +2,13 @@
 
 # Fishing Pole - ability to bait, cast, reel, unhook
 class FishingPole
-
   def initialize
     @baited = false
     @cast = false
     user_prompt
   end
 
+  # prompt user to make a selection
   def user_prompt
     puts ''
     puts 'What would you like to do?'
@@ -18,24 +18,24 @@ class FishingPole
     user_selection
   end
 
+  # method to parse the selections made by the user
   def user_selection
     case @user_selection
-      when 'B'
-        bait
-      when 'C'
-        cast
-      when 'R'
-        reel
-      when 'U'
-        unhook
-      else
-        puts 'Please make another selection.'
-        user_prompt
+    when 'B'
+      bait
+    when 'C'
+      cast
+    when 'R'
+      reel
+    when 'U'
+      unhook
+    else
+      puts 'Please make another selection.'
+      user_prompt
     end
-
-
   end
 
+  # determine what choices should be displayed
   def user_choices
     if @reel
       puts '(U)nhook'
@@ -52,23 +52,25 @@ class FishingPole
     end
   end
 
-
+  # bait action
   def bait
     @baited = true
     puts 'You bait your hook.'
     user_prompt
   end
 
+  # cast action
   def cast
     @cast = true
     puts 'You cast your hook into the lake.'
     user_prompt
   end
 
+  # reel action - if the hook was not baited, user won't reel in a fish
   def reel
     @reel = true
     puts 'You reel in your hook.'
-    unless @baited
+    if @baited == false
       puts "You didn't catch anything."
     else
       puts 'You caught a lovely trout.  Good for you.'
@@ -76,10 +78,10 @@ class FishingPole
     end
   end
 
+  # unhook action
   def unhook
     puts 'You unhook your fish.'
   end
-
 end
 
-pole = FishingPole.new
+FishingPole.new
