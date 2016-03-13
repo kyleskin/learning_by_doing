6facaec2eff77eb0285dbf1436fe2ce151fe87a4
Kyle Skinner <kyleskin@gmail.com>
2016-03-12 18:38:20 -0700

    Fishing Pole
    * Created fishing pole class with user input
    
    [#114616043]

--- objects/fishing_pole.rb
+++ objects/fishing_pole.rb
@@ -10,6 +10,7 @@ class FishingPole
   end
 
   def user_prompt
+    puts ''
     puts 'What would you like to do?'
     user_choices
     print '> '
@@ -27,34 +28,52 @@ class FishingPole
         reel
       when 'U'
         unhook
+      else
+        puts 'Please make another selection.'
+        user_prompt
     end
 
 
   end
 
   def user_choices
-    unless @baited
+    if @reel
+      puts '(U)nhook'
+    elsif @cast
+      puts '(R)eel'
+    elsif @baited
+      puts '(C)ast'
+    elsif @baited == false
       puts '(B)ait hook?'
-    end
-
-    unless @cast
       puts '(C)ast?'
+    else
+      puts 'Try another selection'
+      user_prompt
     end
   end
 
 
   def bait
-    @bait = true
+    @baited = true
     puts 'You bait your hook.'
+    user_prompt
   end
 
   def cast
     @cast = true
     puts 'You cast your hook into the lake.'
+    user_prompt
   end
 
   def reel
+    @reel = true
     puts 'You reel in your hook.'
+    unless @baited
+      puts "You didn't catch anything."
+    else
+      puts 'You caught a lovely trout.  Good for you.'
+      user_prompt
+    end
   end
 
   def unhook
